f97fb18cf4df1467e4049b1bfd4ae6f5ecea352f
Ryan <ryanmitchell.dev@gmail.com>
2021-03-07 22:57:06 +0000

    start dashboard page

--- Noteworthy/Startup.cs
+++ Noteworthy/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Noteworthy.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
